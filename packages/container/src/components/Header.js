d0f409d49bbd9ff57f886b3b05960a2929551f44
Victor E González P <vemiliogp@gmail.com>
2024-10-06 01:53:43 +0200

    Added header

---
+++ packages/container/src/components/Header.js
@@ -0,0 +1,56 @@
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Button from "@mui/material/Button";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import { Link as RouterLink } from "react-router-dom";
+
+export default function Header({ signedIn, onSignOut }) {
+  const onClick = () => {
+    if (signedIn && onSignOut) {
+      onSignOut();
+    }
+  };
+
+  return (
+    <>
+      <AppBar
+        position="static"
+        color="default"
+        elevation={0}
+        sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
+      >
+        <Toolbar
+          sx={{
+            flexWrap: "wrap",
+            justifyContent: "space-between",
+          }}
+        >
+          <Typography
+            variant="h6"
+            color="inherit"
+            noWrap
+            component={RouterLink}
+            to="/"
+            sx={{ textDecoration: "none" }}
+          >
+            App
+          </Typography>
+          <Button
+            color="primary"
+            variant="outlined"
+            component={RouterLink}
+            to={signedIn ? "/" : "/auth/signin"}
+            onClick={onClick}
+            sx={{
+              margin: (theme) => theme.spacing(1, 1.5),
+              textDecoration: "none",
+            }}
+          >
+            {signedIn ? "Logout" : "Login"}
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+}
