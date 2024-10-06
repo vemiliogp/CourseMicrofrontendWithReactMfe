a2c86041b09b26894e00c481f828886f0bfedb13
Victor E González P <vemiliogp@gmail.com>
2024-10-06 15:09:34 +0200

    Randomize production classname

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,29 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 
+const theme = createTheme({});
+const cache = createCache({
+  key: "co",
+  prepend: true,
+});
+
 export default () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
-    </BrowserRouter>
+    <CacheProvider value={cache}>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <div>
+            <Header />
+            <MarketingApp />
+          </div>
+        </BrowserRouter>
+      </ThemeProvider>
+    </CacheProvider>
   );
 };
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,23 +1,31 @@
 import React from "react";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
 
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
 const theme = createTheme({});
+const cache = createCache({
+  key: "ma",
+  prepend: true,
+});
 
 export default () => {
   return (
     <div>
-      <ThemeProvider theme={theme}>
-        <BrowserRouter>
-          <Routes>
-            <Route exact path="/pricing" element={<Pricing />} />
-            <Route path="/" element={<Landing />} />
-          </Routes>
-        </BrowserRouter>
-      </ThemeProvider>
+      <CacheProvider value={cache}>
+        <ThemeProvider theme={theme}>
+          <BrowserRouter>
+            <Routes>
+              <Route exact path="/pricing" element={<Pricing />} />
+              <Route path="/" element={<Landing />} />
+            </Routes>
+          </BrowserRouter>
+        </ThemeProvider>
+      </CacheProvider>
     </div>
   );
 };
